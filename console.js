357613a44d5ddde34a2bd17b3711c1d439caf173
unscodst <spikester@sbcglobal.net>
2019-02-05 19:22:39 -0800

    Console Logging
    Console log warmup

---
+++ console.js
@@ -0,0 +1,15 @@
+console.log('Hello World')
+console.log(123)
+console.log(true)
+var greeting = 'world';
+console.log(greeting)
+console.log([1,2,3,4])
+console.log({a:1,b:2})
+console.table({a:1,b:2})
+console.error('Error')
+console.warn('Warning')
+console.time('Time')
+for(i=0;i<10;i++) {
+    console.log(i)
+}
+console.timeEnd('Time')
