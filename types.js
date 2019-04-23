0389321ab28d3e926c911a375e6489c8ef4bb412
unscodst <spikester@sbcglobal.net>
2019-04-22 21:04:48 -0700

    Type Conversion

---
+++ types.js
@@ -0,0 +1,46 @@
+// PRIMITIVE
+
+// STRING
+const name = 'John Doe';
+console.log("Name: ", typeof name);
+
+// NUMBER
+const age = 45;
+console.log("Age: ", typeof age);
+
+// BOOLEAN
+const hasCar = true;
+console.log("hasCar: ", typeof hasCar);
+
+// NULL
+const car = null;
+console.log("Car: ", typeof car);
+
+// UNDEFINED
+let check;
+console.log("Check: ", typeof check);
+
+// SYMBOL
+const sym = Symbol();
+console.log("Sym: ", typeof sym);
+
+
+// REFERENCE TYPES - Objects
+
+// ARRAY
+const hobbies = ['movies','music'];
+console.log("Hobbies: ", typeof hobbies);
+
+// OBJECT LITERAL
+const address = {
+    city: 'Boston',
+    state: 'MA'
+};
+console.log("Address: ", typeof address);
+
+// DATE
+const today = new Date();
+console.log("Today: ", typeof today);
+
+
+
