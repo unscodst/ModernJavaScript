13322263da402fa0ec6c7554033f9d21ffbde707
unscodst <spikester@sbcglobal.net>
2019-06-07 16:20:30 -0700

    Sub Classes
    Here we make sub classes by extending another class. We also make a static method for setting the membership cost.

---
+++ section-five/sub_classes.js
@@ -0,0 +1,29 @@
+document.querySelector('.title').textContent = 'Sub Classes';
+
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    greeting() {
+        return `Hello ${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Customer extends Person {
+    constructor(firstName,lastName,phone,membership) {
+        super(firstName,lastName);
+        this.phone = phone;
+        this.membership = membership;
+    }
+
+    static getMembershipCost() {
+        return 500;
+    }
+}
+
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+
+console.log(john);
+console.log(john.greeting())
+console.log(Customer.getMembershipCost())
