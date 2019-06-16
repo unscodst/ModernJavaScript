13f14a30839ed30d71f607957e3808deb12767b0
unscodst <spikester@sbcglobal.net>
2019-06-15 22:48:32 -0700

    Promises
    We use a Promise instead of callbacks for a modern way of blocking calls.

--- section-seven/app.js
+++ section-seven/app.js
@@ -1,3 +1,40 @@
+//#region Promises
+// const posts = [
+//     {title: 'Post One', body:'This is post one'},
+//     {title: 'Post Two', body:'This is post two'}
+// ];
+
+// function createPost(post) {
+//     return new Promise(function(resolve, reject){
+//         setTimeout(function() {
+//             posts.push(post);
+//             const error = true;
+//             if(!error) {
+//                 resolve();
+//             } else reject('Error: Something went wrong')
+            
+//         },2000)
+//     })
+// }
+
+// function getPosts() {
+//     setTimeout(function() {
+//         let output = '';
+//         posts.forEach(function(post) {
+//             output += `
+//                 <li>${post.title}</li>
+//             `;
+//         });
+//         document.body.innerHTML = output;
+//     },1000)
+// }
+// // Promises are called after the initial function is resolved.
+// createPost({title: 'Post Three', body: 'This is post three'})
+// .then(getPosts)
+// .catch(function(error) {
+//     console.log(error);
+// })
+//#endregion
 
 //#region Callback Function
 // const posts = [
@@ -27,7 +64,7 @@
 // getPosts();
 
 
-// Callback - A function passed into another function and then called.
+// // Callback - A function passed into another function and then called.
 // function createPost(post,callback) {
 //     setTimeout(function() {
 //         posts.push(post);
